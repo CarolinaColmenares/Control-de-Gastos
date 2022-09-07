4537977d1baf2bc0901b82c25bec919ada80ce30
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-07 18:26:20 -0300

    Añadir un botón para Registrar un Nuevo gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
-  const [isValidPresupuesto, setIsValidPresupuesto] = useState (false)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
 
   return (
@@ -16,6 +17,17 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img
+            src={IconoNuevoGasto}
+            alt='icono nuevo gasto'
+          />
+        </div>
+      )
+      }
+
     </div>
   )
 }
